8ecd283022e789a7c4986b20a2a2a742319395e3
Daniel Hood <dhood@apple.com>
2015-05-13 22:39:37 -0700

    [FEATURE] adds expose parameter to add emscripten sdk to PATH

--- bin/lurk.js
+++ bin/lurk.js
@@ -2,8 +2,20 @@
 var path = require('path');
 var program = require('commander');
 var pkg = require(path.resolve(__dirname, '..', 'package.json'));
+var exec = require('child_process').exec;
+var emsdkDir = path.resolve(__dirname, '..', 'emsdk');
 
- 
 program
   .version(pkg.version)
+  .option('e, expose', 'sets the emsdk environment variables')
   .parse(process.argv);
+
+if (program.expose) {
+  var setEnv = exec([
+    path.join(emsdkDir, 'emsdk_env.sh'),
+    '&&',
+    path.join(emsdkDir, 'emsdk_set_env.sh')
+  ].join(' '), function (error, stdout, stderr) {
+    console.log(stdout);
+  });
+}
