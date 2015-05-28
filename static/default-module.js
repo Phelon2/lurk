fa233ffd4d31227bf1972b67fadf07d3067cf925
Daniel Hood <hood.danielc@gmail.com>
2015-05-28 06:49:57 -0700

    [FEATURE] calls canvas resize event when window is resized in default module implementation and allows custom implementation

--- static/default-module.js
+++ static/default-module.js
@@ -1,3 +1,13 @@
 var Module = {
   canvas: this.window ? document.getElementById('canvas') : null
 };
+
+if (Module.canvas) {
+  window.onresize = function () {
+    if (typeof Module.setCanvasSize === 'function') {
+      var width = Module.canvas.clientWidth;
+      var height = Module.canvas.clientHeight;
+      Module.setCanvasSize(width, height);
+    }
+  }
+}
